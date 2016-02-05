ca9a78890d54a1de275b004adab84234c0486e14
Kevin <yangkevint@gmail.com>
2016-02-06 05:49:10 +0900

    Added in NYT API call

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 function loadData() {
 
     var $body = $('body');
@@ -14,13 +13,30 @@ function loadData() {
 
     var streetVal = $street.val();
     var cityVal = $city.val();
-    
+
     // load streetview
-    var picUrl = "http://maps.googleapis.com/maps/api/streetview?size=600x400&location=" + streetVal + ", " +cityVal;
+    var picUrl = "http://maps.googleapis.com/maps/api/streetview?size=600x400&location=" + streetVal + ", " + cityVal;
     console.log(picUrl);
 
+    var nytURL = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + streetVal + ", " + cityVal + "&api-key=74cce60d8b5f70ebab13fa0b5a80f113:5:59978738";
     // YOUR CODE GOES HERE!
     $body.append('<img class="bgimg" src="' + picUrl + ' "/>');
+
+    $.getJSON(nytURL, function (data) {
+
+        console.log(data);
+        var items = data.response.docs;
+
+        for(var i = 0; i < items.length; i++){
+            var item = items[i];
+            $nytElem.append('<li class ="article">' + '<a href="' + item.web_url + '">' + item.headline.main + '</a>' + '<p>' + item.snippet + '</p>' + '</li>');
+        }
+        $("<ul/>", {
+            "class": "my-new-list",
+            html: items.join("")
+        }).appendTo("body");
+    });
+
     return false;
 }
 
